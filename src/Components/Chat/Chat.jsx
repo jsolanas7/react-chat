77ecb5f7ef3a5494997146f5fe59793f711eec30
Juancito <juanisolanas@gmail.com>
2020-03-11 18:05:00 -0300

    Add environment

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom";
 import { scroller } from "react-scroll";
 import './Chat.css';
-import {sendMessages, getMessages, getUserDataFromToken} from './chatService';
+import { sendMessages, getMessages, getUserDataFromToken } from './chatService';
+import {urlSocket} from '../../enviroment/environment';
 
 const io = require('socket.io-client');
-const socket = io('http://localhost:3001');
+const socket = io(urlSocket);
 
 const Chat = () => {
     // const listMessages = [" 24/11/2019 18:30  | Juan : Hola", "chau", "que tal"];
@@ -12,14 +14,20 @@ const Chat = () => {
     const [firstItems, setFirstItems] = useState('');
     const [messagesList, setMessagesList] = useState([]);
     const [messageInput, setMessageInput] = useState('');
+    const [userLogged, setUserLogged] = useState('');
 
-   
+
     const scrollToBottom = () => {
         var objDiv = document.getElementById("divChat");
-        objDiv.scrollTop = objDiv.scrollHeight;
+        if(objDiv){
+            objDiv.scrollTop = objDiv.scrollHeight;
+        }
     }
-    useEffect(async ()  => {
+    useEffect(async () => {
+        
         await getAll();
+        const userValues = getUserDataFromToken();
+        setUserLogged(userValues.fullName);
         scrollToBottom();
         return () => {
             console.log('finish get all');
@@ -28,17 +36,17 @@ const Chat = () => {
 
     const getAll = async () => {
         const resp = await getMessages();
-        if(resp.length > 0 && resp){
+        if (resp.length > 0 && resp) {
             const userValues = getUserDataFromToken();
-            
-            const arrayMessages = resp.map((item,i) => {
+
+            const arrayMessages = resp.map((item, i) => {
                 let classMessages;
-                if(userValues.fullName == item.user){
+                if (userValues.fullName == item.user) {
                     classMessages = 'chatMessage chatMessageOwner';
-                }else{
+                } else {
                     classMessages = 'chatMessage';
                 }
-                return({
+                return ({
                     owner: item.user,
                     message: item.message,
                     date: item.date,
@@ -52,14 +60,14 @@ const Chat = () => {
         }
     }
     useEffect(() => {
-        socket.on('recibirMensaje',async payload => {
+        socket.on('recibirMensaje', async payload => {
             const userValues = getUserDataFromToken();
             let classMessages;
-                if(userValues.fullName == payload.user){
-                    classMessages = 'chatMessage chatMessageOwner';
-                }else{
-                    classMessages = 'chatMessage';
-                }
+            if (userValues.fullName == payload.user) {
+                classMessages = 'chatMessage chatMessageOwner';
+            } else {
+                classMessages = 'chatMessage';
+            }
             setMessagesList(oldArray => [...oldArray, {
                 owner: payload.user,
                 message: payload.message,
@@ -71,39 +79,38 @@ const Chat = () => {
                 console.log('finish use effect');
             }
         });
-      }, [receiveMessage]);
+    }, [receiveMessage]);
 
 
 
     const send = async () => {
-        if(messageInput != ''){
+        if (messageInput != '') {
             await sendMessages(messageInput);
             setMessageInput('');
         }
-        
-    }
 
+    }
     return (
         <div className="chatContainer form-control">
-            <h2>Chat</h2>
+            <h2>Chat - {userLogged}</h2>
             <div className="listContainer form-control" id="divChat">
                 {
-                    messagesList.length > 0 ? 
-                    messagesList.map(message => {
-                    return(<React.Fragment>
-                        <div className="principalMessageContainer">
-                        <div className="chatOwner">{message.owner}</div>
-                        <div title={message.date} id={message.idScroll} className={message.classes}>{message.message}</div>
-                        </div>
-                    </React.Fragment>)
-                })
-                :
-                <div>No hay mensajes</div>
-                
-            }
+                    messagesList.length > 0 ?
+                        messagesList.map(message => {
+                            return (<React.Fragment>
+                                <div className="principalMessageContainer">
+                                    <div className="chatOwner">{message.owner}</div>
+                                    <div title={message.date} id={message.idScroll} className={message.classes}>{message.message}</div>
+                                </div>
+                            </React.Fragment>)
+                        })
+                        :
+                        <div>No hay mensajes</div>
+
+                }
             </div>
             <div className="inputContainer">
-                <input value={messageInput} onChange={e => setMessageInput(e.target.value) } className="form-control"></input>
+                <input value={messageInput} onChange={e => setMessageInput(e.target.value)} className="form-control"></input>
                 <button onClick={() => send()} className="btn btn-info">Enviar</button>
             </div>
         </div>
--- src/enviroment/environment.js
+++ src/enviroment/environment.js
@@ -1,5 +1,6 @@
 
-const urlSocket = 'http://localhost:3001';
+// const urlSocket = 'http://localhost:3001';
+const urlSocket = 'https://chat-react-pokemon-backend.herokuapp.com';   
 
 
 module.exports = {
